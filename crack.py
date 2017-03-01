095c67f6fdf1f3172ab4ec50891d56603429c929
Tyler Berry <tyler@berryac.com>
2017-03-01 09:43:11 -0700

    Checkpoints to a file
    The program may be run in pieces this way

--- crack.py
+++ crack.py
@@ -8,13 +8,27 @@ import string
 from multiprocessing import Pool as ThreadPool
 
 MIN_PASSWD_LEN = 3
-MAX_PASSWD_LEN = 10
+MAX_PASSWD_LEN = 5
 POSSIBLE_CHARS = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+CHECKPOINT_FILE = 'checkpoint'
+CHECKPOINT_NUM = 10000
 
 def first(iterable):
     """Like `any`, but returns the first truthy value in `iterable`. Returns
     `None` if `any` would return false."""
     return next((item for item in iterable if item), None)
+
+
+def consume(iterator, n):
+    """Advance the iterator n-steps ahead. If n is none, consume entirely."""
+    # Use functions that consume iterators at C speed.
+    if n is None:
+        # feed the entire iterator into a zero-length deque
+        collections.deque(iterator, maxlen=0)
+    else:
+        # advance to the empty slice starting at position n
+        next(it.islice(iterator, n, n), None)
 
 
 def encrypt(hash_algo_num, salt, passwd):
@@ -26,12 +40,51 @@ def encrypt(hash_algo_num, salt, passwd):
     return passwd_hash
 
 
-def possible_passwds(min_length, max_length, possible_chars):
+def _possible_passwds(min_length, max_length, possible_chars):
     """Generates all possible combinations of `possible_chars` between
     `min_length` and `max_length` (inclusive) characters long."""
     for i in range(min_length, max_length + 1):
         for cmb in it.product(possible_chars, repeat=i):
-            yield ''.join(cmb)
+            cmb_str = ''.join(cmb)
+            yield cmb_str
+
+
+def possible_passwds(min_length, max_length, possible_chars):
+    last_chkpt = load_checkpoint()
+    print('starting from:', last_chkpt)
+    iterator = iter(_possible_passwds(min_length, max_length, possible_chars))
+    consume(iterator, last_chkpt)
+    acc = 0
+    for passwd in iterator:
+        if acc == CHECKPOINT_NUM:
+            acc = 0
+            write_checkpoint()
+        else:
+            acc += 1
+        yield passwd
+
+
+def write_checkpoint():
+    """Increments the `CHECKPOINT_FILE` by `CHECKPOINT_NUM`"""
+    last_chkpt = load_checkpoint()
+    with open(CHECKPOINT_FILE, 'w') as f:
+        new_chkpt = last_chkpt + CHECKPOINT_NUM
+        f.write(str(new_chkpt))
+
+
+def load_checkpoint():
+    """Creates or reads a number from the `CHECKPOINT_FILE`"""
+    try:
+        f = open(CHECKPOINT_FILE, 'r')
+        line = f.readline()
+        return int(line) if line else 0
+    except FileNotFoundError:
+        with open(CHECKPOINT_FILE, 'w') as f:
+            f.write(str(0))
+            f.flush()
+            return 0
+    finally:
+        f.close()
 
 
 class ShadowLine(object):
@@ -72,9 +125,13 @@ class ShadowLine(object):
         Returns: The password if a matching password was found; None otherwise
         """
         with ThreadPool() as p:
-            passwd = first(p.imap_unordered(
-                self._test_passwd,
-                possible_passwds(min_pass_len, max_pass_len, possible_chars)))
+            passwd = first(
+                    p.imap_unordered(
+                        self._test_passwd,
+                        possible_passwds(
+                            min_pass_len,
+                            max_pass_len,
+                            possible_chars)))
             if passwd is not None:
                 return passwd
         return None
@@ -149,6 +206,7 @@ def main():
     # TODO: fancy CLI parsing
     filename = os.sys.argv[1]
     crack_shadow_file(filename)
+    os.remove(CHECKPOINT_FILE)
 
 if __name__ == '__main__':
     main()
